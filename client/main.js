afd98af471bf43f0e24e7ec0cbf39a34308c4f87
vylion <volfaria@gmail.com>
2015-07-02 12:12:34 +0200

    Players' positions and colors are synched. There is handling of player disconnection.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,13 @@
 var serverURL = 'localhost:9000'
 var socket = require('socket.io-client')(serverURL)
+//var KeyboardJS = require('./KeyboardJS.js')
+
+var debug = true;
+var preventer = function (evt) {
+//if (condition) evt.preventDefault();
+};
+
+var keyboard = new KeyboardJS(debug, preventer);
 
 // You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
 // which will try to choose the best renderer for the environment you are in.
@@ -10,20 +18,20 @@ document.body.appendChild(renderer.view);
 
 // You need to create a root container that will hold the scene you want to draw.
 var stage = new PIXI.Container();
+stage.alpha = 0.9;
+renderer.backgroundColor = 0xFFFFFF;
 
 // This creates a texture from a 'bunny.png' image.
 var bunnyTexture = PIXI.Texture.fromImage('bunny.png');
 var bunny = new PIXI.Sprite(bunnyTexture);
 global.bunny = bunny
-var otherBunnies = {}
+bunny.tint = Math.floor(Math.random()*16777215)
+var bunnies = {}
 
 // Setup the position and scale of the bunny
 bunny.position.x = Math.random() * 800
 bunny.position.y = Math.random() * 600
 bunny.anchor.set(0.5, 0.5)
-
-// Add the bunny to the scene we are building.
-stage.addChild(bunny);
 
 // kick off the animation loop (defined below)
 animate();
@@ -31,26 +39,80 @@ animate();
 function animate() {
     // start the timer for the next animation loop
     requestAnimationFrame(animate);
+		
+		if(keyboard.char('W')) {
+			bunny.position.y -= 2;
+			var info = bunny.position
+			info.color = bunny.tint
+			socket.emit('update_position', info)
+		}
+		if (keyboard.char('S')) {
+			bunny.position.y += 2
+			var info = bunny.position
+			info.color = bunny.tint
+			socket.emit('update_position', info)
+		}
+		if (keyboard.char('A')) {
+			bunny.position.x -= 2
+			var info = bunny.position
+			info.color = bunny.tint
+			socket.emit('update_position', info)
+		}
+		if (keyboard.char('D')) {
+			bunny.position.x += 2
+			var info = bunny.position
+			info.color = bunny.tint
+			socket.emit('update_position', info)
+		}
+		
     // this is the main render call that makes pixi draw your container and its children.
     renderer.render(stage);
 }
 
-socket.on('update_position', function (pos) {
-  var sprite = otherBunnies[pos.id]
-  if (!sprite) {
-    sprite = new PIXI.Sprite(bunnyTexture)
-    stage.addChild(sprite)
-    otherBunnies[pos.id] = sprite
-    sprite.anchor.set(0.5, 0.5)
-  }
-  sprite.position.x = pos.x
-  sprite.position.y = pos.y
-})
-
 socket.on('connect', function () {
   console.log('connected')
-  socket.emit('update_position', bunny.position)
+	var info = bunny.position
+	info.color = bunny.tint
+  socket.emit('update_position', info)
 })
+
+socket.on('init_position', function (people) {
+	for (var key in people) {
+		sprite = new PIXI.Sprite(bunnyTexture)
+		stage.addChild(sprite)
+		bunnies[people[key].id] = sprite
+		sprite.tint = people[key].color
+		sprite.anchor.set(0.5, 0.5)
+		sprite.position.x = people[key].x
+		sprite.position.y = people[key].y
+	}
+	console.log('Initialized all bunnies')
+	// Add the bunny to the scene we are building.
+	stage.addChild(bunny);
+})
+
+socket.on('update_position', function (info) {
+	var sprite = bunnies[info.id]
+	if(!sprite) {
+		sprite = new PIXI.Sprite(bunnyTexture)
+		stage.addChild(sprite)
+		bunnies[info.id] = sprite
+		sprite.tint = info.color
+		sprite.anchor.set(0.5, 0.5)
+	}
+	console.log(info)
+	sprite.position.x = info.x
+	sprite.position.y = info.y
+})
+
+socket.on('rm_position', function (info) {
+	stage.removeChild(bunnies[info.id])
+	delete bunnies[info.id]
+	console.log('Removed bunny', info.id)
+})
+
+//coins[i])
+
 // npm install --save browserify
 //
 // npm run <script-name>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 var port = process.env.PORT || 9000
 var io = require('socket.io')(port)
 
+var people = {}
+
 io.on('connection', function (socket) {
-  socket.broadcast.emit('hi')
-  console.log('connection', socket.id)
+  socket.broadcast.emit('welcome')
+  console.log('user connected', socket.id)
+	socket.emit('init_position', people)
   socket.on('disconnect', function () {
-    console.log('disconnection', socket.id)
+    console.log('user disconnected', socket.id)
+		socket.broadcast.emit('rm_position', people[socket.id])
+		delete people[socket.id]
   })
-  socket.on('update_position', function (pos) {
-    pos.id = socket.id
-    socket.broadcast.emit('update_position', pos)
+  socket.on('update_position', function (info) {
+    console.log('Updating position', socket.id)
+		info.id = socket.id
+		people[socket.id] = info
+		socket.broadcast.emit('update_position', info)
   })
 })
 
